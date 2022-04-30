13c4a47006a081c2ca1f89d28d9637fa9bb05edc
Maksim Peskov <kostarsf@gmail.com>
2022-04-30 16:48:32 +0300

    Format Card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,30 +1,30 @@
 import st from './Card.module.css';
 
 type Props = {
-    title: string;
-    description: string;
-    date: string;
+  title: string;
+  description: string;
+  date: string;
 }
 
 const Card: React.FC<Props> = ({
-    title,
-    description,
-    date,
+  title,
+  description,
+  date,
 }) => {
-    const isNew = false;
+  const isNew = false;
 
-    return (
-        <div className={"card " + st.card} style={{ cursor: 'pointer' }}>
-            <div className="card-body">
-                <h5 className="card-title">
-                    {title}
-                </h5>
+  return (
+    <div className={"card " + st.card} style={{ cursor: 'pointer' }}>
+      <div className="card-body">
+        <h5 className="card-title">
+          {title}
+        </h5>
 
-                <p className="card-text">{description}</p>
-                <p className="text-primary fs-5 mb-0 d-flex justify-content-between align-items-end"><span>{date}</span> {isNew && <span className="badge bg-primary">Новое</span>}</p>
-            </div>
-        </div>
-    );
+        <p className="card-text">{description}</p>
+        <p className="text-primary fs-5 mb-0 d-flex justify-content-between align-items-end"><span>{date}</span> {isNew && <span className="badge bg-primary">Новое</span>}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
