fef54587d5ccd615ca5dd8e39754e6c70d32dd9e
Maksim Peskov <kostarsf@gmail.com>
2022-05-01 00:53:43 +0300

    Reformat code

--- src/utils/RelativeTime.ts
+++ src/utils/RelativeTime.ts
@@ -10,20 +10,20 @@ export class RelativeTime {
 
     var minute = 60,
       hour = minute * 60,
-      day = hour * 24,
-      week = day * 7;
+      day = hour * 24;
+      // week = day * 7;
 
     var fuzzy;
 
     if (delta < minute) {
       fuzzy = 'только что';
-    } else if (Math.floor(delta / minute) == 1) {
+    } else if (Math.floor(delta / minute) === 1) {
       fuzzy = '1 минуту назад';
     } else if (Math.floor(delta / minute) <= 4) {
       fuzzy = Math.floor(delta / minute) + ' минуты назад';
     } else if (delta < hour) {
       fuzzy = Math.floor(delta / minute) + ' минут назад';
-    } else if (Math.floor(delta / hour) == 1) {
+    } else if (Math.floor(delta / hour) === 1) {
       fuzzy = '1 час назад'
     } else if (Math.floor(delta / hour) <= 4) {
       fuzzy = Math.floor(delta / hour) + ' часа назад'
@@ -31,7 +31,7 @@ export class RelativeTime {
       fuzzy = 'Сегодня';
     } else if (delta < day * 2) {
       fuzzy = 'Вчера';
-    } else if (Math.floor(delta / day) == 1) {
+    } else if (Math.floor(delta / day) === 1) {
       fuzzy = Math.floor(delta / day) + ' день назад';
     } else if (Math.floor(delta / day) <= 4) {
       fuzzy = Math.floor(delta / day) + ' дня назад';
