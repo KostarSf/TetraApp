9aa8d35047dcdce07ee6eebb42022916e4790423
Maksim Peskov <kostarsf@gmail.com>
2022-04-30 14:45:45 +0300

    Add routes placeholder

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Outlet/>}>
+        <Route path='dashboard' element={<App />}/>
+        <Route path='task/:taskId' element={<div>taskpage</div>} />
+        <Route path='*' element={<div>404</div>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
