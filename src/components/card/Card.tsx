111778e57a8e594a4c98e06f4acbb3cff8bac836
Maksim Peskov <kostarsf@gmail.com>
2022-04-30 17:09:46 +0300

    Update Card component with new behaviour

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,28 +1,32 @@
+import { Link } from 'react-router-dom';
+import { TaskDto } from '../../data';
+import { RelativeTime } from '../../utils/RelativeTime';
 import st from './Card.module.css';
 
 type Props = {
-  title: string;
-  description: string;
-  date: string;
+  task: TaskDto
 }
 
 const Card: React.FC<Props> = ({
-  title,
-  description,
-  date,
+  task,
 }) => {
-  const isNew = false;
+  const creationDateString = RelativeTime.fromNowOn(task.creationDate);
+  const isNew = RelativeTime.inMinutes(task.creationDate) < 60 * 24;
 
   return (
-    <div className={"card " + st.card} style={{ cursor: 'pointer' }}>
+    <div className={"card " + st.card} style={{ position: 'relative' }}>
       <div className="card-body">
         <h5 className="card-title">
-          {title}
+          {task.title}
         </h5>
 
-        <p className="card-text">{description}</p>
-        <p className="text-primary fs-5 mb-0 d-flex justify-content-between align-items-end"><span>{date}</span> {isNew && <span className="badge bg-primary">Новое</span>}</p>
+        <p className="card-text">{task.description}</p>
+        <p className="text-primary fs-5 mb-0 d-flex justify-content-between align-items-end">
+          <span>{creationDateString}</span>
+          {isNew && <span className="badge bg-primary">Новое</span>}
+        </p>
       </div>
+      <Link to={`/task/${task.id}`} style={{position: 'absolute', inset: '0'}}/>
     </div>
   );
 }
