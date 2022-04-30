241228daffe5cdd61cca9f0e7fd6e1a815b3c946
Maksim Peskov <kostarsf@gmail.com>
2022-05-01 00:50:50 +0300

    Update relative time strings

--- src/utils/RelativeTime.ts
+++ src/utils/RelativeTime.ts
@@ -19,16 +19,22 @@ export class RelativeTime {
         fuzzy = 'только что';
     } else if (Math.floor(delta / minute) == 1) {
         fuzzy = '1 минуту назад';
-    } else if (Math.floor(delta / minute) < 5) {
+    } else if (Math.floor(delta / minute) <= 4) {
         fuzzy = Math.floor(delta / minute) + ' минуты назад';
     } else if (delta < hour) {
         fuzzy = Math.floor(delta / minute) + ' минут назад';
     } else if (Math.floor(delta / hour) == 1) {
         fuzzy = '1 час назад'
+    } else if (Math.floor(delta / hour) <= 4) {
+        fuzzy = Math.floor(delta / hour) + ' часа назад'
     } else if (delta < day) {
         fuzzy = 'Сегодня';
     } else if (delta < day * 2) {
         fuzzy = 'Вчера';
+    } else if (Math.floor(delta / day) == 1) {
+        fuzzy = Math.floor(delta / day) + ' день назад';
+    } else if (Math.floor(delta / day) <= 4) {
+        fuzzy = Math.floor(delta / day) + ' дня назад';
     } else {
         fuzzy = Math.floor(delta / day) + ' дней назад';
     }
