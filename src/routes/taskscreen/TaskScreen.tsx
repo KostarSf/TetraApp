5eca82e9e71d4779f119e8aa302320a1ad7a0477
Maksim Peskov <kostarsf@gmail.com>
2022-05-12 22:05:52 +0300

    Start implementing work task data overview

--- src/routes/taskscreen/TaskScreen.tsx
+++ src/routes/taskscreen/TaskScreen.tsx
@@ -7,7 +7,7 @@ import './taskscreen.css';
 import ChangeBoardButton from '../../components/changeboardbutton/ChangeBoardButton';
 import Fakecrumb from '../../components/fakecrumb/Fakecrumb';
 import DataActions from '../../utils/data/DataActions';
-import { BoardDto, TaskDto } from '../../utils/data/Types';
+import { BoardDto, TaskDto, WorkTaskDto } from '../../utils/data/Types';
 import WorkTaskModal, { WorkTaskModalData } from '../../components/modals/WorkTaskModal';
 
 const TaskScreen: React.FC = () => {
@@ -15,12 +15,15 @@ const TaskScreen: React.FC = () => {
   const [board, setBoard] = useState(DataActions.getBoardByTask(task) as BoardDto);
   const [showWorkDialog, setShowWorkDialog] = useState(false);
   const [workDialogData, setWorkDialogData] = useState<WorkTaskModalData>();
+  const [workTaskData, setWorkTaskData] = useState<WorkTaskDto | undefined>();
 
   const isNew = RelativeTime.inMinutes(task.creationDate) < 60 * 24;
 
   useEffect(() => {
-
-  }, []);
+    if (board.workBoard) {
+      setWorkTaskData(DataActions.getWorkTaskById(task.id));
+    }
+  }, [board]);
 
   const moveTaskAction = (task: TaskDto, board: BoardDto) => {
     DataActions.moveTask(task, board);
@@ -43,8 +46,9 @@ const TaskScreen: React.FC = () => {
         show={showWorkDialog}
         data={workDialogData}
         onClose={() => setShowWorkDialog(false)}
-        onSave={(workTask) => {
-          console.log('yes');
+        onSave={(workTask, task, board) => {
+          DataActions.setWorkTask(workTask);
+          moveTaskAction(task, board);
         }}
       />
       <div>
@@ -68,7 +72,7 @@ const TaskScreen: React.FC = () => {
                   </>
                 </p>
               </div>
-              <div className='d-flex gap-2'>
+              <div className='d-grid d-sm-flex gap-2'>
                 {DataActions.getAllBoardsWithSort().filter(b => !b.newBoard).map(b => {
                   return (
                     <ChangeBoardButton
@@ -94,22 +98,29 @@ const TaskScreen: React.FC = () => {
               <div className="p-4 m-3 mt-4 border rounded bg-white">
                 <div className='row mb-3'>
                   <div className="col text-secondary">Приоритет: </div>
-                  <div className="col">taskData.rate</div>
+                  <div className="col">{workTaskData?.priority}</div>
                 </div>
                 <div className='row mb-3' >
                   <div className="col text-secondary">Ответств.:</div>
-                  <div className="col">taskData.resp</div>
+                  <div className="col">
+                    {workTaskData && DataActions.getUserById(workTaskData.managerId)?.fullName}
+                  </div>
                 </div>
-                <div className='row flex-wrap' >
+                <div className='row mb-3 flex-wrap' >
                   <div className="col text-secondary">Участники:</div>
                   <div className="col">
                     <ul className='list-group list-group-flush'>
-                      {`taskData.memb.map(i => <li className='list-group-item ps-0'>
-                        {i}
-                      </li>)`}
+                      {workTaskData && DataActions.getUsers().filter(u => workTaskData.memberIds.find(m => m === u.id)).map(u => (
+                        <li key={u.id}>{u.fullName}</li>
+                      ))}
                     </ul>
-                    <button className='btn btn-link'>изменить</button>
                   </div>
+                </div>
+                <div className="row-col d-grid justify-content-end">
+                  <button className='btn btn-link' onClick={() => {
+                    setWorkDialogData({ task, board });
+                    setShowWorkDialog(true);
+                  }}>Редактировать задачу</button>
                 </div>
               </div>
             </div>
