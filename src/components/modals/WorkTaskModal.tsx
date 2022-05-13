87fd034c288be058f6f2ebf3560dd07a8843ffbe
Maksim Peskov <kostarsf@gmail.com>
2022-05-13 03:52:32 +0300

    Move work task form to external component

--- src/components/modals/WorkTaskModal.tsx
+++ src/components/modals/WorkTaskModal.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import DataActions from '../../utils/data/DataActions';
-import { BoardDto, TaskDto, UserDto, WorkTaskDto, WorkTaskPriority } from '../../utils/data/Types';
+import { BoardDto, TaskDto, WorkTaskDto, WorkTaskPriority } from '../../utils/data/Types';
 import useUniqueId from '../../utils/UniqueID';
+import WorkTaskForm from '../worktaskform/WorkTaskForm';
 import Modal from './Modal';
 
 export type WorkTaskModalData = {
@@ -24,23 +25,12 @@ const WorkTaskModal: React.FC<WorkTaskModalProps> = ({
 }) => {
   const [id] = useState(useUniqueId('WorkTaskModal-'))
 
-  const [priority, setPriority] = useState <WorkTaskPriority>(WorkTaskPriority.normal);
-  const [status, setStatus] = useState('Подготовка к выполнению');
-  const [startDate, setStartDate] = useState<Date>(new Date());
-  const [finishDate, setFinishDate] = useState<Date>(() => {
-    let nextDay = new Date(startDate);
-    nextDay.setDate(startDate.getDate() + 1);
-    return nextDay;
-  })
+  const [workTask, setWorkTask] = useState<WorkTaskDto>(getBlankWorkTask());
 
-  const [users, setUsers] = useState<UserDto[]>([]);
-  const [managerId, setManagerId] = useState<number>(DataActions.getCurrentUser().id);
-  const [memberIds, setMemberIds] = useState<number[]>([]);
-
-  useEffect(() => {
-    const users = DataActions.getUsers();
-    setUsers(users);
-  })
+  const onCloseHandle = () => {
+    setWorkTask(getBlankWorkTask());
+    onClose();
+  }
 
   const setId = (name: string) => {
     return `${name}_${id}`
@@ -49,168 +39,54 @@ const WorkTaskModal: React.FC<WorkTaskModalProps> = ({
   const onSaveHandle = () => {
     if (!data) return;
 
-    onSave({
-      taskId: data.task.id,
-      priority,
-      managerId,
-      memberIds,
-      status,
-      startDate: startDate.toISOString(),
-      finishDate: finishDate.toISOString()
-    }, data.task, data.board);
+    const newWorkTask = {
+      ...workTask,
+      taskId: data.task.id
+    }
+
+    onSave(newWorkTask, data.task, data.board);
   }
 
   return (
     <Modal
       show={show}
       closeOnSave
-      onClose={onClose}
+      onClose={onCloseHandle}
       onSave={onSaveHandle}
       header={{
         title: 'Провести опрос'
       }}
       footer={{
         closeButtonText: 'Отмена',
-        saveButtonText: 'Взять в работу'
+        saveButtonText: 'Обновить задачу'
       }}
     >
-      <div className="mb-3">
-        <label htmlFor={setId('priority')} className="form-label">Приоритет</label>
-        <select
-          className="form-select"
-          id={setId('priority')}
-          aria-label="Приоритет"
-          value={getEnumEntryByValue(priority, WorkTaskPriority)}
-          onChange={e => setPriority(setEnumByEntry(e.target.value, WorkTaskPriority))}
-        >
-          {getEnumEntries(WorkTaskPriority).map(i => (
-            <option value={i} key={i}>{WorkTaskPriority[i]}</option>
-          ))}
-        </select>
-      </div>
-      <div className="mb-3">
-        <label htmlFor={setId('manager')} className="form-label">Менеджер</label>
-        <select
-          className="form-select"
-          id={setId('manager')}
-          aria-label="Менеджер"
-          value={managerId}
-          onChange={e => setManagerId(+e.target.value)}
-        >
-          {users.filter(u => !memberIds.find(m => m === u.id)).map(u => (
-            <option value={u.id} key={u.id}>{u.fullName}</option>
-          ))}
-        </select>
-      </div>
-      <div className="mb-3">
-        <label htmlFor={setId('members')} className="form-label">Исполнители</label>
-        <select
-          className="form-select"
-          id={setId('members')}
-          aria-label="Исполнители"
-          value='choose'
-          onChange={e => {
-            const value: string = e.target.value;
-            if (value !== 'choose') {
-              setMemberIds((members) => {
-                return [...members, +value]
-              })
-            }
-          }}
-        >
-          <option value="choose" key='choose'>Выберите из списка</option>
-          {users.filter(u => !(memberIds.find(m => m === u.id) || managerId === u.id)).map(u => (
-            <option value={u.id} key={u.id}>{u.fullName}</option>
-          ))}
-        </select>
-        <div className='bg-light rounded mt-2'>
-          {(memberIds.length === 0) && (
-            <button className='btn btn-link' disabled
-              style={{textDecoration: 'none'}}
-            >
-              Исполнители не выбраны
-            </button>
-          )}
-          {memberIds.map(m => {
-            const user = users.find(u => u.id === m);
-
-            if (!user) return <></>;
-
-            return (
-              <button
-                className='btn btn-link'
-                key={user.id}
-                onClick={e => setMemberIds(members => {
-                  return members.filter(m => m !== user.id)
-                })}
-              >
-                {user.fullName}
-              </button>
-            )
-          })}
-        </div>
-      </div>
-      <div className="mb-3">
-        <label htmlFor={setId('status')} className="form-label">Статус</label>
-        <input
-          type="text"
-          className="form-control"
-          id={setId('status')}
-          placeholder="Статус задачи"
-          value={status}
-          onChange={e => setStatus(e.target.value)}
-        />
-      </div>
-      <div className="mb-3 row">
-        <div className="col-sm-6 mb-3 mb-sm-0">
-          <label htmlFor={setId('startDate')} className="form-label">Дата начала</label>
-          <input
-            type="datetime-local"
-            className="form-control"
-            id={setId('startDate')}
-            value={getLocalTimeValue(startDate)}
-            onChange={e => setStartDate(getUtcTimeFromLocal(e.target.value)) }
-          />
-        </div>
-        <div className="col-sm-6">
-          <label htmlFor={setId('endDate')} className="form-label">Дата завершения</label>
-          <input
-            type="datetime-local"
-            className="form-control"
-            id={setId('endDate')}
-            value={getLocalTimeValue(finishDate)}
-            onChange={e => setFinishDate(getUtcTimeFromLocal(e.target.value))}
-          />
-        </div>
-      </div>
+      <WorkTaskForm
+        value={workTask}
+        onChange={wt => setWorkTask(wt)}
+      />
     </Modal>
   );
 };
 
-const getLocalTimeValue = (date: Date): string => {
-  const now = new Date(date);
-  now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
-  return now.toISOString().slice(0, 16);
-}
+const getBlankWorkTask = (): WorkTaskDto => {
+  const currentDay = new Date();
 
-const getUtcTimeFromLocal = (date: string): Date => {
-  const now = new Date(date);
-  console.log(now);
-  return now;
-}
+  const getNextDay = () => {
+    let nextDay = new Date(currentDay);
+    nextDay.setDate(currentDay.getDate() + 1);
+    return nextDay;
+  }
 
-const getEnumEntryByValue = <EnumType,>(value: EnumType[keyof EnumType], enumType: EnumType ) => {
-  return getEnumEntries(enumType).filter(e => {
-    return enumType[e] === value;
-  })[0]
-}
-
-const setEnumByEntry = <EnumType,>(entry: string, enumType: EnumType) => {
-  return enumType[entry as keyof typeof enumType];
-}
-
-const getEnumEntries = <EnumType,>(enumType: EnumType) => {
-  return (Object.keys(enumType) as Array<keyof typeof enumType>)
+  return {
+    priority: WorkTaskPriority.normal,
+    status: 'Подготовка к выполнению',
+    taskId: -1,
+    managerId: DataActions.getCurrentUser().id,
+    memberIds: [],
+    startDate: currentDay.toISOString(),
+    finishDate: getNextDay().toISOString()
+  }
 }
 
 export default WorkTaskModal;
